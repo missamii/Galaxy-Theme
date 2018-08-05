f364683d8d6edf9750cfe5b52bf21ae7654478a0
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2018-08-05 16:07:01 -0400

    fresh new theme featuring galaxy

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+// vex modal
+// var vex = require('vex-js');
+// vex.registerPlugin(require('vex-dialog'));
+// vex.defaultOptions.className = 'vex-theme-os';
